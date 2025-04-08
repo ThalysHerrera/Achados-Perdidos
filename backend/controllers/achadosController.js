b0e562f9f8b684900576c731841302c2282d9de6
thalys <thalysherrera615@gmail.com>
2025-04-08 15:16:59 -0300

    Achados e Perdidos

--- backend/controllers/achadosController.js
+++ backend/controllers/achadosController.js
@@ -1,53 +1,9 @@
-// const db = require('../db');
-
-// exports.getAll = (req, res) => {
-//   db.query('SELECT * FROM achados', (err, results) => {
-//     if (err) return res.status(500).send(err);
-//     res.json(results);
-//   });
-// };
-
-// exports.create = (req, res) => {
-//   const { nome, descricao, tipo_id, local_encontrado, data_achado } = req.body;
-//   db.query(
-//     'INSERT INTO achados (nome, descricao, tipo_id, local_encontrado, data_achado) VALUES (?, ?, ?, ?, ?)',
-//     [nome, descricao, tipo_id, local_encontrado, data_achado],
-//     (err, result) => {
-//       if (err) return res.status(500).send(err);
-//       res.status(201).send({ id: result.insertId });
-//     }
-//   );
-// };
-
-// exports.update = (req, res) => {
-//   const { nome, descricao, tipo_id, local_encontrado, data_achado } = req.body;
-//   const id = req.params.id;
-//   db.query(
-//     'UPDATE achados SET nome=?, descricao=?, tipo_id=?, local_encontrado=?, data_achado=? WHERE id=?',
-//     [nome, descricao, tipo_id, local_encontrado, data_achado, id],
-//     (err) => {
-//       if (err) return res.status(500).send(err);
-//       res.sendStatus(200);
-//     }
-//   );
-// };
-
-// exports.remove = (req, res) => {
-//   const id = req.params.id;
-//   db.query('DELETE FROM achados WHERE id=?', [id], (err) => {
-//     if (err) return res.status(500).send(err);
-//     res.sendStatus(200);
-//   });
-// };
-
-
-
 
 
 
 const db = require('../db');
 
-// Buscar todos os objetos cadastrados
+
 exports.getAll = (req, res) => {
   db.query('SELECT * FROM achados', (err, results) => {
     if (err) return res.status(500).send(err);
@@ -55,7 +11,6 @@ exports.getAll = (req, res) => {
   });
 };
 
-// Criar novo objeto achado
 exports.create = (req, res) => {
   const {
     nome,
@@ -79,7 +34,7 @@ exports.create = (req, res) => {
   );
 };
 
-// Atualizar objeto existente
+
 exports.update = (req, res) => {
   const {
     nome,
@@ -105,7 +60,7 @@ exports.update = (req, res) => {
   );
 };
 
-// Remover objeto
+
 exports.remove = (req, res) => {
   const id = req.params.id;
   db.query('DELETE FROM achados WHERE id=?', [id], (err) => {
